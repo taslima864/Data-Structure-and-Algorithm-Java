ea460a63b94407c643a08728de2d7c95460a7121
taslima864 <sheikhtaslima00@gmail.com>
2026-05-20 23:32:19 +0530

    Contains duplicate value

---
+++ containsDuplicate.java
@@ -0,0 +1,30 @@
+package Practice_Questions;
+
+import java.util.HashSet;
+
+public class containsDuplicate {
+
+  public static boolean arrayCC(int[] nums) {
+
+    HashSet<Integer> set = new HashSet<>();
+
+    for (int i = 0; i < nums.length; i++) {
+
+      // check duplicate
+      if (set.contains(nums[i])) {
+        return true;
+      } else {
+        set.add(nums[i]);
+      }
+    }
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+
+    int[] nums = { 1, 2, 3, 3, 4, 5, 8, 8, 0 };
+
+    System.out.println(arrayCC(nums));
+  }
+}
