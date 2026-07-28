13997ff9d4ed4ae3397a505e2c54217d1635b9c4
taslima864 <sheikhtaslima00@gmail.com>
2026-07-28 20:31:39 +0530

    Matrix of Diagonal Sum

---
+++ Arrays/DiagonalSum.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class DiagonalSum {
+
+  public static int diagonalSum(int matrix[][]) {
+
+    int sum = 0;
+
+    for (int i = 0; i < matrix.length; i++) {
+
+      // Primary diagonal
+      sum += matrix[i][i];
+
+      // Secondary diagonal
+      sum += matrix[i][matrix.length - 1 - i];
+    }
+
+    return sum;
+  }
+
+  public static void main(String[] args) {
+
+    int matrix[][] = {
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
+        { 13, 14, 15, 16 }
+    };
+
+    System.out.println(diagonalSum(matrix));
+  }
+}
