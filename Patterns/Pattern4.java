56c44c73b2938deec43b383c90f4443eb4cfe4a0
taslima864 <sheikhtaslima00@gmail.com>
2026-08-04 15:44:43 +0530

    Pattern 4

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.*;
+
+public class Pattern4 {
+  public static void solution(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(j);
+      }
+      System.out.println(" ");
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the num of row:");
+    int n = sc.nextInt();
+    solution(n);
+    sc.close();
+
+  }
+}
