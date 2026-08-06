4df406f44a7ed14ac7aa6a44209b1c84b706650f
taslima864 <sheikhtaslima00@gmail.com>
2026-08-06 22:34:16 +0530

    Check String is a Palindrome or not

--- Patterns/Pattern16.java
+++ Patterns/Pattern16.java
@@ -2,7 +2,7 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class Pattern15 {
+public class Pattern16 {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
@@ -10,10 +10,14 @@ public class Pattern15 {
     int n = sc.nextInt();
     sc.close();
 
-    for (int i = 0; i < n; i++) {
-      char ch = (char) ('A' + i);
-      for (int j = 0; j <= i; j++) {
-        System.out.print(ch);
+    for (int i = 0; i <= n; i++) {
+
+      for (int j = 0; j <= n * i - 2; j++) {
+
+        System.out.print(" * ");
+      }
+      for (int j = i; j > n; j--) {
+        System.out.print(" ");
       }
 
       System.out.println();
--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -5,7 +5,8 @@ public class StringCompression {
     String newStr = "";
     for (int i = 0; i < str.length(); i++) {
       Integer count = 1;
-      while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+      while (i < str.length() - 1 &&
+          str.charAt(i) == str.charAt(i + 1)) {
         count++;
         i++;
       }
