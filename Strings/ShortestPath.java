c87af98db33211c54c7073acc5830819f8502f90
taslima864 <sheikhtaslima00@gmail.com>
2026-08-07 00:21:21 +0530

    Approach Shortest Path

---
+++ Strings/ShortestPath.java
@@ -0,0 +1,26 @@
+package Strings;
+
+public class ShortestPath {
+  public static float getShortestPath(String path) {
+    int x = 0, y = 0;
+    for (int i = 0; i < path.length(); i++) {
+      char dir = path.charAt(i);
+      if (dir == 'N') {
+        y++;
+      } else if (dir == 'S') {
+        y--;
+      } else if (dir == 'W') {
+        x--;
+      } else if (dir == 'E') {
+        x++;
+      }
+    }
+    return (float) Math.sqrt(x * x + y * y);
+  }
+
+  public static void main(String[] args) {
+    String path = "WNEENESENNN";
+    System.out.println(getShortestPath(path));
+
+  }
+}
