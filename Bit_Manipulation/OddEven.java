9f17d8f84e1c2d2eb2b3c163e03ad4776fa0aba9
taslima864 <sheikhtaslima00@gmail.com>
2026-08-09 20:10:07 +0530

    Check Odd and Even number

---
+++ Bit_Manipulation/OddEven.java
@@ -0,0 +1,22 @@
+package Bit_Manipulation;
+
+import java.util.*;
+
+public class OddEven {
+  public static void solution(int n) {
+    int bitMask = 1;
+    if ((n & bitMask) == 0) {
+      // even number
+      System.out.println("even number");
+    } else {
+      System.out.println("odd number");
+
+    }
+  }
+
+  public static void main(String args[]) {
+    solution(3);
+    solution(3);
+    solution(14);
+  }
+}
