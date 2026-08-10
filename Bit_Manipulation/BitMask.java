e7e1fbbaa50b3eebf763e24c650afc1e6f7ed2bd
taslima864 <sheikhtaslima00@gmail.com>
2026-08-10 12:10:59 +0530

    Clear ith bit

--- Bit_Manipulation/BitMask.java
+++ Bit_Manipulation/BitMask.java
@@ -1,6 +1,7 @@
 package Bit_Manipulation;
 
 public class BitMask {
+  // get ith Bit
   public static int solution(int n, int i) {
     int bitMask = 1 << i;
     if ((n & bitMask) == 0) {
@@ -8,17 +9,25 @@ public class BitMask {
     } else {
       return 1;
     }
-
   }
 
+  // set ith Bit
   public static int solution2(int n, int i) {
     int bitMask = 1 << i;
     return (n | bitMask);
   }
 
+  // clear ith bit
+  public static int solution3(int n, int i) {
+    int bitMask = ~(1 << i);
+    return n & bitMask;
+
+  }
+
   public static void main(String args[]) {
     // System.out.print(solution(13, 1));
-    System.out.println(solution2(10, 2));
+    // System.out.println(solution2(10, 2));
+    System.out.println(solution3(10, 1));
 
   }
 }
