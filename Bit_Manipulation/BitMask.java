c137cdf7db785b918440aef78e5854dc8e6d318c
taslima864 <sheikhtaslima00@gmail.com>
2026-08-11 17:56:15 +0530

    Clear last i bits

--- Bit_Manipulation/BitMask.java
+++ Bit_Manipulation/BitMask.java
@@ -29,11 +29,19 @@ public class BitMask {
     return n | BitMask;
   }
 
+  // clear last i bits
+  public static int solution5(int n, int i) {
+    int bitMask = (~0) << i;
+    return n & bitMask;
+
+  }
+
   public static void main(String args[]) {
     // System.out.print(solution(13, 1));
     // System.out.println(solution2(10, 2));
     // System.out.println(solution3(10, 1));
-    System.out.println(solution4(10, 2, 1));
+    // System.out.println(solution4(13, 2, 1));
+    System.out.println(solution5(15, 2));
 
   }
 }
