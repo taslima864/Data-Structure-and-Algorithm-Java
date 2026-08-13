b938ccb20a290679c4a3f586a3bd329a8b92ed90
taslima864 <sheikhtaslima00@gmail.com>
2026-08-13 21:44:07 +0530

    Add 1 to an Integer Using Bit Manipulation

--- Bit_Manipulation/SwapNum.java
+++ Bit_Manipulation/SwapNum.java
@@ -1,12 +1,20 @@
 package Bit_Manipulation;
 
 public class SwapNum {
-  public static void main(String[] args) {
-    int x = 3, y = 4;
+  public static void solution(int x, int y) {
     System.out.println("Before swap: x = " + x + " and y = " + y);
     x = x ^ y;
     y = x ^ y;
     x = x ^ y;
     System.out.println("After swap: x = " + x + " and y = " + y);
+  }
+
+  public static void solution2(int x) {
+    System.out.println(x + " + " + 1 + " is " + -~x);
+  }
+
+  public static void main(String[] args) {
+    // solution(3, 4);
+    solution2(6);
   }
 }
