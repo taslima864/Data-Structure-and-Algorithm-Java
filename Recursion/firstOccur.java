826864145461de5b92b18ab3b96574d08e52458c
taslima864 <sheikhtaslima00@gmail.com>
2026-08-16 23:04:00 +0530

    first Occurence

---
+++ Recursion/firstOccur.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class firstOccur {
+  public static int solution(int arr[], int key, int i) {
+    if (i == arr.length) {
+      return -1;
+    }
+    if (arr[i] == key) {
+      return i;
+    }
+    return solution(arr, key, i + 1);
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 8, 3, 6, 9, 5, 10, 2, 5, 3 };
+    System.out.println(solution(arr, 5, 0));
+  }
+}
