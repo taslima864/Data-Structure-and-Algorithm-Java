32d0fe35200f6d686da0681ddc81a216ff062d75
taslima864 <sheikhtaslima00@gmail.com>
2026-08-18 21:26:24 +0530

    Optimized power of x

---
+++ Recursion/Power.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class Power {
+  public static int optimizedPower(int a, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    int halfPower = optimizedPower(a, n / 2);
+    int halfPowerSq = halfPower * halfPower;
+
+    // n is odd
+    if (n % 2 != 0) {
+      halfPowerSq = a * halfPowerSq;
+
+    }
+    return halfPowerSq;
+  }
+
+  public static void main(String[] args) {
+    int a = 2;
+    int n = 10;
+    System.out.println(optimizedPower(a, n));
+  }
+}
